45b140287d24fe1cfd86d5fdc7068788281583f1
Somtochukwu Ernest <52139555+Somto811@users.noreply.github.com>
2019-08-06 08:32:35 +0100

    Update stickers.py

--- stdplugins/stickers.py
+++ stdplugins/stickers.py
@@ -44,18 +44,18 @@ async def _(event):
 
     me = borg.me
     userid = event.from_id
-    packname = f"{userid}'s @UniBorg Pack"
-    packshortname = f"Uni_Borg_{userid}"  # format: Uni_Borg_userid
+    packname = f"@loxxi Pack"
+    packshortname = f"loxxi_packs"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
-    file_ext_ns_ion = "@UniBorg_Sticker.png"
+    file_ext_ns_ion = "@loxxi_Sticker.png"
     file = await borg.download_file(reply_message.media)
     uploaded_sticker = None
     if is_a_s:
         file_ext_ns_ion = "AnimatedSticker.tgs"
         uploaded_sticker = await borg.upload_file(file, file_name=file_ext_ns_ion)
-        packname = f"{userid}'s @AnimatedStickersGroup"
-        packshortname = f"Uni_Borg_{userid}_as"  # format: Uni_Borg_userid
+        packname = f"@loxxi AnimatedStickers"
+        packshortname = f"loxxi_animatedPack"  # format: Uni_Borg_userid
     elif not is_message_image(reply_message):
         await event.edit("Invalid message type")
         return
@@ -65,7 +65,7 @@ async def _(event):
             sticker.seek(0)
             uploaded_sticker = await borg.upload_file(sticker, file_name=file_ext_ns_ion)
 
-    await event.edit("Processing this sticker. Please Wait!")
+    await event.edit("Grabbing Sketch Brush & Painting this Picture")
     print(uploaded_sticker)
 
     async with borg.conversation("@Stickers") as bot_conv:
@@ -110,7 +110,7 @@ async def _(event):
             await silently_send_message(bot_conv, sticker_emoji)
             await silently_send_message(bot_conv, "/done")
 
-    await event.edit(f"sticker added! Your pack can be found [here](t.me/addstickers/{packshortname})")
+    await event.edit(f"Painting Done! Find Your Masterpiece [Here](t.me/addstickers/{packshortname})")")
 
 
 @borg.on(admin_cmd("packinfo"))
@@ -126,7 +126,7 @@ async def _(event):
         return
     stickerset_attr_s = rep_msg.document.attributes
     stickerset_attr = find_instance(stickerset_attr_s, DocumentAttributeSticker)
-    if not stickerset_attr.stickerset:
+    if not stickerset_attr.tickerset:
         await event.edit("sticker does not belong to a pack.")
         return
     get_stickerset = await borg(
