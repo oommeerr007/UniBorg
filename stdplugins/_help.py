5a89b1607f69e73c0a270faab052afc9eeb53a11
Dark-Princ3 <54077356+Dark-Princ3@users.noreply.github.com>
2019-08-18 14:36:13 +0530

    Update _help.py

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -12,7 +12,7 @@ async def _(event):
         s_help_string = borg._plugins[splugin_name].__doc__
     else:
         s_help_string = ""
-    help_string = """@UniBorg ( **Custom Built By** @loxxi ) \n**Verified Account**: ✅\n**Official Website**: https://webloaded.com.ng\n
+    help_string = """@UniBorg ( **Custom Built By** @Dark_Princ3 ) \n**Verified Account**: ✅\n**Official Website**: https://webloaded.com.ng\n
 Python {}
 Telethon {}
 
